7eaabe7fdadbb8f2fb00f1cb42510c4e172fa51e
Theo Klautke <tk172@hdm-stuttgart.de>
2024-09-27 23:36:04 +0200

    Added swagger

--- appointment-app/packages/service/src/main.ts
+++ appointment-app/packages/service/src/main.ts
@@ -7,9 +7,20 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+      .setTitle('Appointment Application')
+      .setDescription('An Application for doing appointments')
+      .setVersion('1.0')
+      .addTag('appointments')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
