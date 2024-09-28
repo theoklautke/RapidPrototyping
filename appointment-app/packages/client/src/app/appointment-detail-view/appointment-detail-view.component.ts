908d54bc31f7f278434d5fcbfe1bb236e5149877
Theo Klautke <tk172@hdm-stuttgart.de>
2024-09-28 23:21:09 +0200

    Added post and delete in frontend

--- appointment-app/packages/client/src/app/appointment-detail-view/appointment-detail-view.component.ts
+++ appointment-app/packages/client/src/app/appointment-detail-view/appointment-detail-view.component.ts
@@ -2,18 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppointmentService } from "../appointment.service";
 import { Appointment } from "interfaces";
-import {NgbInputDatepicker, NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import { NgbInputDatepicker, NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-appointment-detail-view',
   standalone: true,
-  imports: [CommonModule, NgbInputDatepicker],
+  imports: [CommonModule, NgbInputDatepicker, FormsModule],
   templateUrl: './appointment-detail-view.component.html',
-  styles: ``,
+  styleUrls: ['./appointment-detail-view.component.scss'],
 })
 export class AppointmentDetailViewComponent implements OnInit {
 
   public appointmentList: Appointment[] = [];
+  public newAppointment: Appointment = {
+    assignment: '',
+    branch: '',
+    vehicleOwner: '',
+    vehicleRegNo: '',
+    status: '',
+    date: '',
+    time: ''
+  };
 
   constructor(
       private readonly appointmentService: AppointmentService,
@@ -27,5 +37,33 @@ export class AppointmentDetailViewComponent implements OnInit {
 
   public open(modal: any): void {
     this.modalService.open(modal);
+  }
+
+  public deleteAppointment(appointmentId: number | undefined): void {
+    this.appointmentList = this.appointmentList.filter(app => app.id !== appointmentId);
+    this.appointmentService.deleteAppointment(appointmentId).subscribe();
+  }
+
+  public saveAppointment(modal: any): void {
+    if (this.newAppointment.assignment && this.newAppointment.branch &&
+        this.newAppointment.vehicleOwner && this.newAppointment.vehicleRegNo &&
+        this.newAppointment.status && this.newAppointment.date && this.newAppointment.time) {
+
+      this.appointmentService.createAppointment(this.newAppointment).subscribe(savedAppointment => {
+        this.appointmentList.push(savedAppointment);
+
+        this.newAppointment = {
+          assignment: '',
+          branch: '',
+          vehicleOwner: '',
+          vehicleRegNo: '',
+          status: '',
+          date: '',
+          time: ''
+        };
+
+        modal.close();
+      });
+    }
   }
 }
--- appointment-app/packages/client/src/app/appointment.service.ts
+++ appointment-app/packages/client/src/app/appointment.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Appointment} from "interfaces";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Appointment } from 'interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppointmentService {
 
+  private apiUrl = 'http://localhost:3000/Api/appointment';
+
   constructor(private http: HttpClient) {}
 
-  private baseURL = 'http://localhost:3000/api/appointment';
-
   public getAppointments(): Observable<Appointment[]> {
-    return this.http.get<Appointment[]>(this.baseURL);
+    return this.http.get<Appointment[]>(this.apiUrl);
   }
 
+  public deleteAppointment(id: number | undefined): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  public createAppointment(appointment: Appointment): Observable<Appointment> {
+    return this.http.post<Appointment>(this.apiUrl, appointment);
+  }
 }
