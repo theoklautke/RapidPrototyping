82ead967acc70b011d7d3ba372e9e55a64aafb19
Theo Klautke <tk172@hdm-stuttgart.de>
2024-11-08 23:03:28 +0100

    Added routes

--- appointment-app/packages/client/src/app/app.routes.ts
+++ appointment-app/packages/client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Route } from '@angular/router';
+import {AppointmentDetailViewComponent} from "./appointment-detail-view/appointment-detail-view.component";
+import {DealerDetailViewComponent} from "./dealer-detail-view/dealer-detail-view.component";
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    { path: 'appointment', component: AppointmentDetailViewComponent },
+    { path: 'dealer', component: DealerDetailViewComponent },
+    { path: '', redirectTo: '/appointment', pathMatch: 'full' },
+    { path: '**', redirectTo: '/appointment' },
+];
--- appointment-app/packages/client/src/app/navbar/navbar.component.ts
+++ appointment-app/packages/client/src/app/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {NgbInputDatepicker} from "@ng-bootstrap/ng-bootstrap";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
     selector: 'app-navbar',
     standalone: true,
-    imports: [CommonModule, NgbInputDatepicker],
+    imports: [CommonModule, NgbInputDatepicker, RouterLink, RouterLinkActive],
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.scss'],
 })
